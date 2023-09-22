e7763ad6a85077e797c6eabe931d6ac9aacb3cb8
enzosamojedny <enzosamojedny@gmail.com>
2023-09-22 14:39:08 -0300

    updates in item responsiveness

--- src/components/body-items/Item.jsx
+++ src/components/body-items/Item.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./ProductItem.css";
 import ResponsiveDialog from "./extras/Popup";
 import { Typography, Box, Button } from "@mui/material";
@@ -29,49 +29,37 @@ function Item({ products }) {
       <div className="product-container">
         {result.map((product, index) => (
           <div key={index} className="product-item">
-            <img
-              src={product.image}
-              alt={product.title}
-              className="product-item__image"
-              style={{ width: 100, height: 100 }}
-            />
-            <Typography
-              variant="h2"
-              style={{ fontSize: 17, fontWeight: 500 }}
-              className="product-item-title"
+            <NavLink
+              to={`/products/${product.id}`}
+              style={{ textDecoration: 'none', color: 'inherit' }}
             >
-              {product.title}
-            </Typography>
-            <Typography variant="h3" style={{ fontSize: 19, fontWeight: 600 }}>
-              ${product.price}
-            </Typography>
-            <Box
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                width: "250px",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <div>
-                <Link to={`/products/${product.id}`}>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    style={{
-                      color: "#000000",
-                      borderColor: "#172738",
-                      marginRight: 20,
-                      backgroundColor: "#E6E6FA",
-                      fontWeight: 600,
-                    }}
-                  >
-                    Details
-                  </Button>
-                </Link>
-              </div>
-            </Box>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-item__image"
+                style={{ width: 100, height: 100 }}
+              />
+              <Typography
+                variant="h2"
+                style={{ fontSize: 17, fontWeight: 500 }}
+                className="product-item-title"
+              >
+                {product.title}
+              </Typography>
+              <Typography variant="h3" style={{ fontSize: 19, fontWeight: 600 }}>
+                ${product.price}
+              </Typography>
+              <Box
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "250px",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+              </Box>
+            </NavLink>
           </div>
         ))}
       </div>
