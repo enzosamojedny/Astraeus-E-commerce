526a7f14b3699f38eeeaac23db38d5f4e6d7877f
enzosamojedny <76167611+enzosamojedny@users.noreply.github.com>
2023-09-23 22:31:25 -0300

    small update

--- src/components/body-items/extras/userContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const UserContext = createContext();
-
-export function useUser() {
-    return useContext(UserContext);
-}
-
-export default UserContext;
